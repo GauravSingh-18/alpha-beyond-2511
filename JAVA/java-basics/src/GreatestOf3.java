ff2ad5c62183c584a7b412b9ebf4be24e20f824c
Alpha Sneha <gauravsingh08032007@gmail.com>
2026-03-03 10:44:12 +0530

    Day 01 [BASICS,IF-ELSE,LOOP]

--- JAVA/java-basics/src/GreatestOf3.java
+++ JAVA/java-basics/src/GreatestOf3.java
@@ -1,24 +1,23 @@
 import java.util.Scanner;
 class GreatestOf3 {
-    public static void main(String[]   args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n1 = sc.nextInt();
         int n2 = sc.nextInt();
         int n3 = sc.nextInt();
+
         if(n1==n2&&n2==n3){
             System.out.println("All numbers are same");
-            return;
         }
-
 
         if(n1>=n2&&n1>=n3){
-            System.out.println("N1 is greatest"+ n1);
+            System.out.println("Greatest is :- "+n1);
         }
-        else if(n2>=n3&&n2>=n1){
-            System.out.println("N2 is greatest"+n2);
+        else if(n2>n3){
+            System.out.println("Greatest is:- "+n2);
         }
         else{
-            System.out.println("N3 is the greatest"+n3);
+            System.out.println("Greatest is :- "+n3);
         }
     }
 }
